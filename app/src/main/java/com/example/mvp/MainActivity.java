41acea905693b07712719669ce5f8c06e7d98409
yguo915 <yguo1@macalester.edu>
2020-11-24 02:52:06 +0800

    Connect to firebase

--- app/src/main/java/com/example/mvp/MainActivity.java
+++ app/src/main/java/com/example/mvp/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestore b = FirebaseFirestore.getInstance();
 
         s1 = getResources().getStringArray(R.array.locations);
         s2 = getResources().getStringArray(R.array.contacts);
