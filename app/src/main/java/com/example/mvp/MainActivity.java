b9f25c1886071a772f81d47183b6c2ba1f4d35d3
RW <ronanlwallace@gmail.com>
2020-11-24 13:18:46 -0800

    Firestore Add User code

--- app/src/main/java/com/example/mvp/MainActivity.java
+++ app/src/main/java/com/example/mvp/MainActivity.java
@@ -7,11 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
+
+        RecyclerView recyclerView;
 
     String s1[], s2[];
     int images[] = {R.drawable.aolinrice, R.drawable.cc, R.drawable.lc};
@@ -33,5 +38,15 @@ public class MainActivity extends AppCompatActivity {
         MyAdaptor myAdaptor = new MyAdaptor(this, s1, s2, images, button);
         recyclerView.setAdapter(myAdaptor);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Create a new user with a first and last name
+        Map<String, Object> userTest = new HashMap<>();
+        userTest.put("first", "Someome different 2.0");
+        userTest.put("last", "Wallace");
+        userTest.put("born", 1999);
+
+        db.collection("users").add(userTest);
+
+
     }
 }
