e01c1ad50a359d94b7a81f1790fb58274719eeb3
RW <ronanlwallace@gmail.com>
2020-11-24 22:13:15 -0800

    Getting Data from Firestore progress

--- app/src/main/java/com/example/mvp/MainActivity.java
+++ app/src/main/java/com/example/mvp/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.mvp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-import com.google.firebase.firestore.FirebaseFirestore;
 
+import com.google.android.gms.common.util.ArrayUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.security.Key;
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
 
-        RecyclerView recyclerView;
+    RecyclerView recyclerView;
 
     String s1[], s2[];
     int images[] = {R.drawable.aolinrice, R.drawable.cc, R.drawable.lc};
     Button button;
+    List<Location> locationData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +55,49 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Create a new user with a first and last name
-        Map<String, Object> userTest = new HashMap<>();
-        userTest.put("first", "Someome different 2.0");
-        userTest.put("last", "Wallace");
-        userTest.put("born", 1999);
+        // Map<String, Object> userTest = new HashMap<>();
+        // userTest.put("first", "Tester");
+        // userTest.put("last", "Tester");
+        // userTest.put("born", 1999);
 
-        db.collection("users").add(userTest);
+        //db.collection("users").add(userTest);
 
-
+        db.collection("locations")
+          .get()
+          .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+              @Override
+              public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                  if (task.isSuccessful()) {
+                      for (QueryDocumentSnapshot document : task.getResult()) {
+                          Log.d("CHECK", document.getId() + " => " + document.getData().values());
+                          Log.d("CHECK STRING", document.getData().get("contact").toString());
+                          Location location = new Location(document.getData().get("name").toString(), document.getData().get("contact").toString());
+                          // locationData.add(location);
+                      }
+                  } else {
+                      Log.w("ERROR", "Error getting documents.", task.getException());
+                  }
+              }
+          });
     }
+
+
+
+
+//    db.collection("locations")
+//      .get()
+//          .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//        @Override
+//        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//            if (task.isSuccessful()) {
+//                for (QueryDocumentSnapshot document : task.getResult()) {
+//                    Log.d("CHECK", document.getId() + " => " + document.getData().values());
+//                }
+//            } else {
+//                Log.w("ERROR", "Error getting documents.", task.getException());
+//            }
+//        }
+//    });
+
+
 }
