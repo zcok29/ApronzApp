252abee7224a9b6a1ac00bc66e614ca174fd93a4
RW <ronanlwallace@gmail.com>
2020-11-24 22:11:51 -0800

    Added Location Class

---
+++ app/src/main/java/com/example/mvp/Location.java
@@ -0,0 +1,15 @@
+package com.example.mvp;
+
+import java.lang.reflect.Array;
+
+public class Location {
+
+        public String contact;
+        public String name;
+
+        public Location(String name, String contact){
+                this.name = name;
+                this.contact = contact;
+        }
+
+}
