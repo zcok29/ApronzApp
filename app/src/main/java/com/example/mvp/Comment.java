1be8d381c7c10a6dde17eafeac3d6bd81dcaba22
RW <ronanlwallace@gmail.com>
2020-12-03 14:38:00 -0800

    Added Comment class
    This class will be used when retrieving the comment data from the database

---
+++ app/src/main/java/com/example/mvp/Comment.java
@@ -0,0 +1,20 @@
+/**
+ * Comment Class
+ *
+ * This class serves as an object for storing comment data that is retrieved
+ * from the database.
+ */
+
+package com.example.mvp;
+
+public class Comment {
+
+        public String content;
+        public String timestamp;
+
+        public Comment(String content, String timestamp){
+                this.content = content;
+                this.timestamp = timestamp;
+        }
+
+}
