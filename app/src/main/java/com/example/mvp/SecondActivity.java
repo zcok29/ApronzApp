f6feeaa11737f5fa0e29c4c6efc67cad6434a97c
RW <ronanlwallace@gmail.com>
2020-12-03 12:05:40 -0800

    Handles comment and timestamp on button click
    When the fab button is clicked, comment text and timestamp are recorded and are available to be stored in the database

--- app/src/main/java/com/example/mvp/SecondActivity.java
+++ app/src/main/java/com/example/mvp/SecondActivity.java
@@ -12,7 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.protobuf.Timestamp;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +24,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Get Firestore database instance
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
         final EditText editText = (EditText) findViewById(R.id.edit_text);
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -38,6 +44,18 @@ public class SecondActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Gets the text from the editText box when the fab button is pressed
+                String comment = editText.getText().toString();
+                //  Gets current Timestamp when the fab button is pressed  (for testing)
+                long epoch = System.currentTimeMillis()/1000;
+                String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (epoch*1000));
+
+                // Logs the comment in Run/Logcat window when the fab button is pressed (for testing)
+                Log.d("Comment: ", comment);
+                // Logs the timestamp in Run/Logcat window when the fab button is pressed  (for testing)
+                Log.d("Epoch Timestamp: ", "" + epoch);
+                Log.d("Date Timestamp: ", date);
+
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
