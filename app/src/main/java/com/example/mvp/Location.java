e68873c0777cb17ec62848f0ea688f08479c4fac
RW <ronanlwallace@gmail.com>
2020-12-04 06:15:13 -0800

    Added documentID attribute
    (fixed small typo)

--- app/src/main/java/com/example/mvp/Location.java
+++ app/src/main/java/com/example/mvp/Location.java
@@ -12,13 +12,13 @@ public class Location {
         public String contact;
         public String name;
         public String address;
-        public String doucmentID;
+        public String documentID;
 
         public Location(String name, String contact, String address, String doucmentID){
                 this.name = name;
                 this.contact = contact;
                 this.address = address;
-                this.doucmentID = doucmentID;
+                this.documentID = doucmentID;
         }
 
 }
